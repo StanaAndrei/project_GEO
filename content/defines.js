6b410fd764ea3d274584cec638f2c4cc386642c9
StanaAndrei <44771163+StanaAndrei@users.noreply.github.com>
2020-05-05 16:34:25 +0300

    improves

---
+++ content/defines.js
@@ -0,0 +1,27 @@
+const footerHTMLstream = 
+"<footer>"+
+
+"<p>prezentare Gyula!"+
+"<br>"+
+"de Drug Erick și Stana Andrei"+
+"</p></footer>";
+
+const headerHTMLstream = '<header>'+
+'<div id="container" style="white-space:nowrap">'+
+
+    '<div id="title_image" style="display:inline;">'+
+        '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Gyula_V%C3%A1r_nappal.jpg/290px-Gyula_V%C3%A1r_nappal.jpg"/>'+
+   '</div>'+
+
+    '<div id = "texts">'+
+        '<h1 id = "main_title" >Gyula</h1>'+
+    '</div>'+
+    '<div id = "aut">'+
+       '<p id="taut">Autori:Stana Andrei, Drug Erick Robert.........</p>'+
+    '</div>'+
+'</div>'+
+'</header>';
+
+var assoc_data = []
+assoc_data["footer"] = footerHTMLstream;
+assoc_data["header"] = headerHTMLstream;
---
+++ content/script.js
@@ -0,0 +1,10 @@
+
+load_HTMLstream = (sval) => {
+    document.getElementById(sval).innerHTML = assoc_data[sval];
+    }
+
+$(document).ready(function() {
+        console.log("work!");
+        load_HTMLstream("header");
+        load_HTMLstream("footer");
+});
